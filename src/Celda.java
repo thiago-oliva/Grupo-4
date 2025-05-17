7c27afa85122cb465e36a01b56b649118221095e
antonavaarro <atronavarro@estudiantes.unsam.edu.ar>
2025-05-17 18:26:31 -0300

    Update Celda.java

--- src/Celda.java
+++ src/Celda.java
@@ -1,34 +1,15 @@
-import java.util.List;
-import java.util.ArrayList;
-
 public class Celda {
-    public List<Object> valor;
+    private Object valor;
 
-
-    public Celda(List<Object> valor){
-        this.valor = new ArrayList<>();
-    }
-
-    public List<Object>  getValor(){
-        return valor;
-    }
-
-    public void setValor(List<Object> valor) {
+    public Celda(Object valor) {
         this.valor = valor;
     }
 
-    public void agregarValor(Object nuevoValor) {
-        valor.add(nuevoValor);
+    public Object getValor() {
+        return valor;
     }
 
- //   public void modificarValor(int indice, Object nuevoValor) {
- //       if (indice >= 0 && indice < valor.size()) {
- //           valor.set(indice, nuevoValor);
- //       } else {
- //               System.out.println("Índice fuera de rango.");
- //       }
-
+    public void setValor(Object valor) {
+        this.valor = valor;
     }
-
-
 }
