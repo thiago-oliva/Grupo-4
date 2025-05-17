02b425ebe686a186f4bf2c8751f0dbe1e0395f1f
antonavaarro <atronavarro@estudiantes.unsam.edu.ar>
2025-05-17 19:05:13 -0300

    Update Tabla.java

--- src/Tabla.java
+++ src/Tabla.java
@@ -14,37 +14,40 @@ public class Tabla {
 
     public Tabla(String nombreTabla, List<Columna> columnas, List<String> etiquetasFilas, List<String> etiquetasColumnas, Map<String, Integer> mapaFilas, Map<String, Integer> mapaColumnas) {
         this.nombreTabla = nombreTabla;
-        this.columnas = new ArrayList<>();
-        this.etiquetasFilas = new ArrayList<>();
-        this.etiquetasColumnas = new ArrayList<>();
-        this.mapaFilas = new HashMap<>();
-        this.mapaColumnas = new HashMap<>();
+        this.columnas = new ArrayList<>(columnas);
+        this.etiquetasFilas = new ArrayList<>(etiquetasFilas);
+        this.etiquetasColumnas = new ArrayList<>(etiquetasColumnas);
+        this.mapaFilas = new HashMap<>(mapaFilas);
+        this.mapaColumnas = new HashMap<>(mapaColumnas);
     }
 
     public int getCantidadColumnas() {
         return columnas.size();
     }
 
-    public int getCantidadfilas() {
-        return columnas.get(0).getCantidadFilas();
+    public List<String> getEtiquetasFilas() {
+        return new ArrayList<>(etiquetasFilas); 
     }
 
-    public List<String> getetiquetasFilas() {
-        return etiquetasFilas;
+    public List<String> getEtiquetasColumnas() {
+        return new ArrayList<>(etiquetasColumnas);
     }
 
-    public List<String> getetiquetasColumnas() {
-        return etiquetasColumnas;
+    public List<Columna> getColumnas() {
+        return new ArrayList<>(columnas);
     }
 
+    public Celda getCelda(String etiquetaFila, String etiquetaColumna) {
+        int fila = mapaFilas.get(etiquetaFila);
+        int columna = mapaColumnas.get(etiquetaColumna);
+        return columnas.get(columna).getCelda(fila);
+    }
 
-
-
-
-
-
-
-
+    public void setCelda(String etiquetaFila, String etiquetaColumna, Object valor) throws Columna.excepcionTipoDato {
+        int fila = mapaFilas.get(etiquetaFila);
+        int columna = mapaColumnas.get(etiquetaColumna);
+        columnas.get(columna).setCelda(fila, valor);
+    }
 
 
 }
