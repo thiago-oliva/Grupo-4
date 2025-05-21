1900910da457f1cbbd62e6f04c7e7f4c16e27ad4
Franest-222 <frangevs2@gmail.com>
2025-05-21 16:37:38 -0300

    Agrego comentarios

--- src/Celda.java
+++ src/Celda.java
@@ -4,11 +4,11 @@ public class Celda {
     public Celda(Object valor) {
         this.valor = valor;
     }
-
+    // getter: obtiene el valor almacenado en una celda
     public Object getValor() {
         return valor;
     }
-
+    // setter: modifica el valor almacenado en una celda
     public void setValor(Object valor) {
         this.valor = valor;
     }
