5032019a566635e73cc07efdbca92f1d3bf2d693
antonavaarro <atronavarro@estudiantes.unsam.edu.ar>
2025-06-03 16:25:16 -0300

    Subo cambios de Celda y Columna

--- src/Celda.java
+++ src/Celda.java
@@ -1,15 +1,33 @@
-public class Celda {
-    private Object valor;
+//Con generics
+public class Celda<T> {
+    private T valor;
 
-    public Celda(Object valor) {
+    public Celda(T valor) {
         this.valor = valor;
     }
-    // getter: obtiene el valor almacenado en una celda
-    public Object getValor() {
+
+    public T getValor() {
         return valor;
     }
-    // setter: modifica el valor almacenado en una celda
-    public void setValor(Object valor) {
+
+    public void setValor(T valor) {
         this.valor = valor;
     }
 }
+
+//Con object
+//public class Celda {
+//    private Object valor;
+//
+//    public Celda(Object valor) {
+//        this.valor = valor;
+//    }
+//    // getter: obtiene el valor almacenado en una celda
+//    public Object getValor() {
+//        return valor;
+//    }
+//    // setter: modifica el valor almacenado en una celda
+//    public void setValor(Object valor) {
+//        this.valor = valor;
+//    }
+//}
