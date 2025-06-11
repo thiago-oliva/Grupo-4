0ee1a5aecf1128ce78337cc286025051b65924f8
antonavaarro <atronavarro@estudiantes.unsam.edu.ar>
2025-06-11 18:37:07 -0300

    1

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,6 @@
 import java.util.*;
 import java.util.function.Predicate;
 
-import java.util.*;
-import java.util.function.Predicate;
-
 public class Main {
 
     public static void main(String[] args) {
